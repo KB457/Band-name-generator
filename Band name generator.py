c2f498f8edd1704f7ddff59cd408b56fbea854b8
KB457 <77336245+KB457@users.noreply.github.com>
2021-06-08 16:47:14 +0100

    Add files via upload

---
+++ Band name generator.py
@@ -0,0 +1,18 @@
+print("Hello friend")
+city=input("What city are your from?\n")
+pet=input("What is your pets name?\n")
+print("Your band name could be" + " " + city+ " "+ pet)
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
